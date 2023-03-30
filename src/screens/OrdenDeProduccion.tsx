1faab7b9f10c6e5a044fe14307496c44d32cac96
JohanP151 <jas.pavon@yavirac.edu.ec>
2023-03-29 21:28:02 -0500

    fecha

--- src/screens/OrdenDeProduccion.tsx
+++ src/screens/OrdenDeProduccion.tsx
@@ -1,10 +1,14 @@
-import React, { } from 'react';
-import { SafeAreaView, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, SafeAreaView, ScrollView, Text, TextInput, View } from 'react-native';
 import { styles } from '../themes/ProduccionTheme';
 import { BotonesFinal } from '../components/BotonesFinal';
+import DatePicker from 'react-native-date-picker';
 
 
 export const OrdenDeProduccion = () => {
+  const [date, setDate] = useState(new Date())
+  const [open, setOpen] = useState(false)
+
   return (
     <View>
       <SafeAreaView>
@@ -12,22 +16,28 @@ export const OrdenDeProduccion = () => {
           <View style={styles.cabecera} />
           <Text style={styles.texto}>Orden de Producción</Text>
           <Text style={styles.linea}>__________________________________________</Text>
-          <Text style={styles.nombre}>Nombre</Text>
+          <Text style={styles.nombre}>Número del Documento</Text>
+          <TextInput style={styles.infoDocumento}>Production</TextInput>
+          <Text style={styles.nombre}>Fecha</Text>
+          <Button title='Open' onPress={() => setOpen(true)}/>
+          <DatePicker 
+            modal
+            open={open}
+            date={date}
+            onConfirm={(date) =>{
+              setOpen(false)
+              setDate(date)
+            }}
+            onCancel={() => {
+              setOpen(false)
+            }}
+          />
+          <Text style={styles.descripcion}>Producto</Text>
           <TextInput style={styles.infoDocumento}>Production</TextInput>
           <Text style={styles.atributos}>Atributos</Text>
           <View style={styles.cuadroAtributos} >
-            <Text style={styles.textoAtributos}>Nombre                 Descripcion                 Cantidad</Text>
+            <Text style={styles.textoAtributos}>Producto                                                    Cantidad</Text>
           </View>
-          <Text style={styles.descripcion}>Descripción</Text>
-          <TextInput style={styles.infoDocumento}>Production</TextInput>
-          <Text style={styles.nombreTabla}>Nombre de la tabla</Text>
-          <TextInput style={styles.infoDocumento}>Production</TextInput>
-          <Text style={styles.generarServer}>Generar servidor</Text>
-          <TouchableOpacity>
-            <View style={styles.generarBot}>
-              <Text style={styles.generarBotText}>GENERAR SERVIDOR</Text>
-            </View>
-          </TouchableOpacity>
           <View style={styles.fila}>
             <BotonesFinal texto="Guardar" color="#0A8EBC" />
             <BotonesFinal texto="Cancelar" color="#000000" />
